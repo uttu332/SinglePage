aebc09418136c909ca8ead6d24b56b7051c20b68
uttu332 <inbox.utkarsh@gmail.com>
2023-10-27 12:37:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+ import React from "react";
+import { BrowserRouter,Route,Routes,Link} from "react-router-dom"
 import './App.css';
+
+function HomePage(){
+  return(
+    <div>
+      HomePage
+    </div>
+  )
+}
+function AboutPage(){
+  return(
+    <div>
+      AboutPage
+    </div>
+  )
+}
+
+function NotFoundPage(){
+  return(
+    <div>
+      NotFoundPage
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <header>
+     <nav>
+       <ul>
+        <li>
+          <Link to="/">HomePage</Link>
+        </li>
+        <li>
+          <Link to="/about">AboutPage</Link>
+        </li>
+       </ul>
+     </nav>
+   </header>
+     <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/about" element={<AboutPage />}/>
+        <Route path="/*" element={<NotFoundPage />}/>
+     </Routes>
+   </BrowserRouter>
   );
 }
 
